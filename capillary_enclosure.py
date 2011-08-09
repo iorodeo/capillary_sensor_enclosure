6896a3b24bf52f3ae93cfd92f1980a73f768f299
Will Dickson <will@iorodeo.com>
2011-08-08 19:03:56 -0700

    Project creation.

---
+++ capillary_enclosure.py
@@ -0,0 +1,205 @@
+import scipy
+from py2scad import *
+
+
+class Capillary_Enclosure(Basic_Enclosure):
+
+    def __init__(self,params):
+        self.params = params
+        self.add_sensor_mount_holes()
+        self.add_capillary_holes()
+        self.add_cable_hole()
+        super(Capillary_Enclosure,self).__init__(self.params)
+
+    def make(self):
+        super(Capillary_Enclosure,self).make()
+        self.make_rubber_band_notch()
+        self.make_backlight_standoffs()
+        self.make_led_plate()
+
+    def get_assembly(self,**kwargs):
+        parts_list = super(Capillary_Enclosure,self).get_assembly(**kwargs)
+
+        led_plate = self.led_plate
+        parts_list.append(led_plate)
+        return parts_list
+
+    def add_capillary_holes(self):
+        """
+        Add holes for capillary positioning
+        """
+        hole_list = [] 
+        hole_diam = self.params['capillary_hole_diam']
+        hole_offset_x, hole_offset_y = self.params['capillary_hole_offset']
+        x,y,z = self.params['inner_dimensions']
+
+        panel_list= ('left', 'right')
+        for panel in panel_list:
+            x_pos, y_pos = hole_offset_x,-0.5*z + 0.5*hole_diam + hole_offset_y
+            hole = {
+                    'panel'    : panel,
+                    'type'     : 'round',
+                    'location' : (x_pos, y_pos),
+                    'size'     : hole_diam,
+                    }
+            hole_list.append(hole)
+        self.params['hole_list'].extend(hole_list)
+
+    def add_sensor_mount_holes(self):
+        """
+        Add mount holes for sensor
+        """
+        hole_list = [] 
+        hole_diam = self.params['sensor_mount_hole_diam']
+        hole_space = self.params['sensor_mount_hole_space']
+        for i in (-1,1):
+            x_pos, y_pos = i*0.5*hole_space, 0.0
+            hole = {
+                    'panel'    : 'bottom', 
+                    'type'     : 'round', 
+                    'location' : (x_pos, y_pos),
+                    'size'     : hole_diam,
+                    }
+            hole_list.append(hole)
+        self.params['hole_list'].extend(hole_list)
+
+    def add_cable_hole(self):
+        hole_list = [] 
+        hole_width = self.params['cable_hole_width']
+        x,y,z = self.params['inner_dimensions']
+
+        x_pos = 0
+        y_pos = -0.5*z + 0.5*hole_width 
+        hole = {
+                'panel'     : 'front',
+                'type'      : 'round',
+                'location'  : (x_pos, y_pos),
+                'size'      : hole_width,
+                }
+        hole_list.append(hole)
+
+        x_pos = 0
+        y_pos = -0.5*z
+        hole = {
+                'panel'    : 'front', 
+                'type'     : 'square', 
+                'location' : (x_pos, y_pos), 
+                'size'     : (hole_width, hole_width),
+                }
+        hole_list.append(hole)
+
+        self.params['hole_list'].extend(hole_list)
+
+    def make_rubber_band_notch(self):
+        hole_list = []
+        hole_width = self.params['rubber_band_notch_width']
+        sensor_space = self.params['sensor_mount_hole_space']
+        x,y,z = self.params['inner_dimensions']
+
+        # Create holes for bottom panel
+        for i in (-1,1):
+            for j in (-1,1):
+
+                # Round portion of hole
+                x_pos = i*0.5*sensor_space
+                y_pos = j*(0.5*self.bottom_y - 0.5*hole_width)
+                hole = {
+                        'panel'    : 'bottom',
+                        'type'     : 'round', 
+                        'location' : (x_pos, y_pos),
+                        'size'     : hole_width,
+                        }
+                hole_list.append(hole)
+
+                # Rectangular portion of hole
+                y_pos = j*0.5*self.bottom_y
+                hole = {
+                        'panel'    : 'bottom',
+                        'type'     : 'square', 
+                        'location' : (x_pos, y_pos), 
+                        'size'     : (hole_width, hole_width),
+                        }
+                hole_list.append(hole)
+
+        #Create holes for front and back panels
+        panel_list = ('front' , 'back')
+        for panel in panel_list:
+            for i in (-1,1): 
+                # Round portion of hole
+                x_pos = i*0.5*sensor_space
+                y_pos = -(0.5*z - 0.5*hole_width)
+                hole = {
+                        'panel'    : panel,
+                        'type'     : 'round',
+                        'location' : (x_pos, y_pos),
+                        'size'     : hole_width,
+                        }
+                hole_list.append(hole)
+
+                # Square portion of hole
+                y_pos = -0.5*z
+                hole = {
+                        'panel'    : panel,
+                        'type'     : 'square', 
+                        'location' : (x_pos, y_pos),
+                        'size'     : (hole_width, hole_width),
+                        }
+                hole_list.append(hole)
+
+        self.add_holes(hole_list)
+
+
+    def make_backlight_standoffs(self):
+        hole_list = []
+        hole_diam = self.params['standoff_hole_diameter']
+        x_pos_list = [x for x,y in self.standoff_xy_pos]
+
+        
+        self.backlight_standoff_xy = []
+        for x_pos in x_pos_list:
+            y_pos = 0
+            self.backlight_standoff_xy.append((x_pos,y_pos))
+            hole = {
+                    'panel'    : 'top',
+                    'type'     : 'round',
+                    'location' : (x_pos, y_pos),
+                    'size'     : hole_diam,
+                    }
+            hole_list.append(hole)
+
+        self.add_holes(hole_list)
+
+    def make_led_plate(self):
+        wall_thickness = self.params['wall_thickness']
+        plate_x, plate_y = self.params['led_plate_xy']
+        hole_diam = self.params['standoff_hole_diameter']
+        sensor_length = self.params['sensor_length']
+
+        self.led_plate = Cube(size=(plate_x,plate_y,wall_thickness))
+
+        # Add standoff holes
+        hole_list = []
+        for x_pos, y_pos in self.backlight_standoff_xy:
+            hole = {
+                    'panel'    : 'led_plate',
+                    'type'     : 'round',
+                    'location' : (x_pos, y_pos),
+                    'size'     : hole_diam,
+                    }
+            hole_list.append(hole)
+
+        # Add led holes
+        
+
+        self.add_holes(hole_list)
+
+
+
+
+
+
+
+        
+
+
+
---
+++ make_enclosure.py
@@ -0,0 +1,62 @@
+"""
+Creates an enclosure
+"""
+from py2scad import *
+from capillary_enclosure import Capillary_Enclosure
+
+INCH2MM = 25.4
+
+# Inside dimensions
+x,y,z = 70.0, 45.0, 1.25*INCH2MM 
+hole_list = []
+
+params = {
+        'inner_dimensions'         : (x,y,z), 
+        'wall_thickness'           : 1.5, 
+        'lid_radius'               : 3.0,  
+        'top_x_overhang'           : 3.0,
+        'top_y_overhang'           : 3.0,
+        'bottom_x_overhang'        : 3.0,
+        'bottom_y_overhang'        : 3.0, 
+        'lid2front_tabs'           : (0.25,0.75),
+        'lid2side_tabs'            : (0.25, 0.75),
+        'side2side_tabs'           : (0.5,),
+        'lid2front_tab_width'      : 7.0,
+        'lid2side_tab_width'       : 7.0, 
+        'side2side_tab_width'      : 7.0,
+        'standoff_diameter'        : 0.25*INCH2MM,
+        'standoff_offset'          : 0.05*INCH2MM,
+        'standoff_hole_diameter'   : 0.116*INCH2MM, 
+        'hole_list'                : hole_list,
+        'sensor_mount_hole_diam'   : 0.11*INCH2MM,
+        'sensor_mount_hole_space'  : 57.40, 
+        'capillary_hole_diam'      : 0.98,  
+        'capillary_hole_offset'    : (0.25,3.26+0.5),
+        'cable_hole_width'         : 5.0, 
+        'rubber_band_notch_width'  : 2.0, 
+        'led_plate_xy'             : (x-3.0,y-17.0),
+        'led_slot_xy'              : (3.02,1.5), 
+        'led_num'                  : 6, 
+        'sensor_length'            : 48.0,
+        }
+
+enclosure = Capillary_Enclosure(params)
+enclosure.make()
+
+part_assembly = enclosure.get_assembly(
+        show_top=False,
+        show_bottom=False, 
+        explode=(0,0,0)
+        )
+
+part_projection = enclosure.get_projection()
+
+prog_assembly = SCAD_Prog()
+prog_assembly.fn = 50
+prog_assembly.add(part_assembly)
+prog_assembly.write('enclosure_assembly.scad')
+
+prog_projection = SCAD_Prog()
+prog_projection.fn = 50
+prog_projection.add(part_projection)
+prog_projection.write('enclosure_projection.scad')
